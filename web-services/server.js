b97e26c0906788b695a5d7f812769d88554767a9
sunny <sunnyyorita@gmail.com>
2018-08-01 13:35:33 -0700

    added functionality to query the book database

--- web-services/server.js
+++ web-services/server.js
@@ -14,4 +14,6 @@ app.use(morgan('dev'));
 
 app.get('/api/version', (req, res) => res.status(200).send(pkg.version));
 
+require('./lib/search.js')(app, nconf.get('es'));
+
 app.listen(nconf.get('port'), () => console.log('Ready.'));
