901735853825a90c1c333250f42715f3ad1fdc1b
sunny <sunnyyorita@gmail.com>
2018-08-06 19:16:09 -0700

    added error handling code for bad request and server errors

--- web-services/lib/user_booklist.js
+++ web-services/lib/user_booklist.js
@@ -14,36 +14,39 @@ module.exports = (app, es) => {
       `http://${es.host}:${es.port}/${es.books_index}/book/${pgid}`;
 
    const ReadStatus = Object.freeze({
-      "READING": 'Reading',
-      "FINISHED": 'Finished',
-      "BACKLOG": 'BackLog',
-      "ONHOLD": 'On-Hold',
-      "DROPPED": 'Dropped'
+      'READING': 'Reading',
+      'FINISHED': 'Finished',
+      'BACKLOG': 'BackLog',
+      'ONHOLD': 'On-Hold',
+      'DROPPED': 'Dropped'
    });
 
+   /* credit:
+   https://strongloop.com/strongblog/async-error-handling-expressjs-es7-promises-generators/
+   */
+   // default error handling wrapper for async functions
+   let rejectHandler = fn => (...args) => fn(...args).catch(args[2]);
 
    // Add book to the user's list of books
-   // status query string must be ALL CAPS, default value is READING
-   // /api/user/yoshino703/books/pg703?status="READING"
-   app.put('/api/user/:id/books/:pgid', async(req, res, next) => {
+   /* Example: /api/user/yoshino703/books/pg703?status=reading */
+   app.put('/api/user/:id/books/:pgid', (req, res, next) => {
    
       // extract url data and query from request and store them
       const userUrl = getUserUrl(req.params.id);
       const bookUrl = getBookUrl(req.params.pgid);
       req.urlData = {userUrl, bookUrl};
 
-      let readStatus = ReadStatus[req.query.status];
-      if(readStatus === undefined) readStatus = ReadStatus["READING"];
+      let readStatus = ReadStatus[req.query.status.toUpperCase()];
+      if(readStatus === undefined) res.status(400).send('Invalid status!');
       req.queryData = {readStatus};
       next();
-   
-   }, async(req, res, next) => {
+   }, rejectHandler( async(req, res, next) => {
 
-      try {
-         // request user and book in parallel
-         const userOptions = {url: req.urlData.userUrl, json: true};
-         const bookOptions = {url: req.urlData.bookUrl, json: true};
+      // request user and book in parallel
+      const userOptions = {url: req.urlData.userUrl, json: true};
+      const bookOptions = {url: req.urlData.bookUrl, json: true};
          
+      try {
          const [userRes, bookRes] = await Promise.all([
             rp(userOptions),
             rp(bookOptions),
@@ -54,37 +57,30 @@ module.exports = (app, es) => {
          const {_source: book} = bookRes;
 
          req.dbData = Object.freeze({version, user, book});
-         req.pendingUpdate = Object.freeze({
-            id: req.params.pgid,
-            title: book.title,
-            status: req.queryData.readStatus
-         });
       } catch (esResErr) {
          res.status(esResErr.statusCode || 502).json(esResErr.error);
-      }
+      };
+      req.pendingUpdate = Object.freeze({
+         id: req.params.pgid,
+         title: req.dbData.book.title,
+         status: req.queryData.readStatus
+      });
       next();
-   }, async(req, res, next) => {
-
-      const book_null = req.dbData.user.books
-         .findIndex(bookToAdd => bookToAdd === null);
-      if(book_null !== -1) {
-         req.dbData.user.books.splice(book_null, 1);
-      }
+   }), rejectHandler( async(req, res, next) => {
+      
       // check to see if book is already in user's list
       const book_index = req.dbData.user.books
          .findIndex(bookToAdd => bookToAdd.id === req.params.pgid);
-
       if(book_index === -1) {
          // add to the user's book list if not found
-         req.dbData.user.books.push(req.pendingUpdate);
-      
+         req.dbData.user.books.push(req.pendingUpdate);      
       } else {
          // update the book's readStatus
          req.dbData.user.books.splice(book_index, 1, req.pendingUpdate);
       }
-      console.log(req.dbData.user.books.length);
+
+      // put the updated bundle back in the index
       try {
-         // put the updated bundle back in the index
          const version = req.dbData.version;
          const esResBody = await rp.put( {
             url: req.urlData.userUrl,
@@ -96,6 +92,5 @@ module.exports = (app, es) => {
       } catch (esResErr) {
          res.status(esResErr.statusCode || 502).json(esResErr.error);
       }
-   });
-
+   }));
 }
